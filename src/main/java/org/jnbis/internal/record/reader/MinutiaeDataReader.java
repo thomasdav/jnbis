7a181b7eac51a4f6d1fdceff8bd6cceec41842db
David Thomas <thomasdav@gmail.com>
2018-12-07 13:42:04 +0800

    Add missing fields to MinutiaeData and MinutiaeDataReader.

--- src/main/java/org/jnbis/internal/record/reader/MinutiaeDataReader.java
+++ src/main/java/org/jnbis/internal/record/reader/MinutiaeDataReader.java
@@ -21,21 +21,46 @@ public class MinutiaeDataReader extends RecordReader {
         while (buffer.hasRemaining()) {
             Field field = nextField(token);
 
+            if (field == null) {
+                continue;
+            }
+
             String value = field.asString();
 
             switch (field.fieldNumber) {
-            case 1:
-                minutiaeData.setLogicalRecordLength(field.asInteger());
-                break;
-            case 2:
-                minutiaeData.setIdc(Integer.parseInt(value));
-                break;
-            case 3:
-                minutiaeData.setImpressionType(value);
-                break;
-            case 4:
-                minutiaeData.setMinutiaeFormat(value);
-                break;
+                case 1:
+                    minutiaeData.setLogicalRecordLength(field.asInteger());
+                    break;
+                case 2:
+                    minutiaeData.setIdc(Integer.parseInt(value));
+                    break;
+                case 3:
+                    minutiaeData.setImpressionType(value);
+                    break;
+                case 4:
+                    minutiaeData.setMinutiaeFormat(value);
+                    break;
+                case 5:
+                    minutiaeData.setOriginatingFingerprintReadingSystem(value);
+                    break;
+                case 6:
+                    minutiaeData.setFingerPosition(value);
+                    break;
+                case 7:
+                    minutiaeData.setFingerprintPatternClassification(value);
+                    break;
+                case 8:
+                    minutiaeData.setCorePosition(value);
+                    break;
+                case 9:
+                    minutiaeData.setDeltaPosition(value);
+                    break;
+                case 10:
+                    minutiaeData.setNumberOfMinutiae(value);
+                    break;
+                case 12:
+                    minutiaeData.setMinutiaeAndRidgeCountData(value);
+                    break;
             }
         }
         
